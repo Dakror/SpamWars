e60eddcd1a40e31224591b55de07ee9f17038da7
Dakror <mail@dakror.de>
2014-05-29 21:03:45 +0200

    "Starting over" due to heavy structure rework

--- src/main/java/de/dakror/spamwars2/SpamWars.java
+++ src/main/java/de/dakror/spamwars2/SpamWars.java
@@ -3,10 +3,14 @@ package de.dakror.spamwars;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
+import de.dakror.dakrorbin.DakrorBin;
 import de.dakror.dakrorbin.Launch;
 import de.dakror.gamesetup.util.Helper;
 import de.dakror.spamwars.game.Game;
+import de.dakror.spamwars.game.UpdateThread;
 import de.dakror.spamwars.game.weapon.Part;
+import de.dakror.spamwars.layer.MenuLayer;
+import de.dakror.spamwars.net.User;
 import de.dakror.spamwars.settings.CFG;
 
 /**
@@ -24,9 +28,7 @@ public class SpamWars
 		{
 			e.printStackTrace();
 		}
-		// Launch.init(args);
-		Launch.username = "Dakror";
-		Launch.pwdMd5 = "hi";
+		Launch.init(args);
 		CFG.INTERNET = Helper.isInternetReachable();
 		
 		CFG.init();
@@ -41,5 +43,23 @@ public class SpamWars
 		Part.init();
 		
 		new Game();
+		Game.user = new User(Launch.username, null, 0);
+		
+		Game.currentFrame.init("Spam Wars");
+		try
+		{
+			DakrorBin.init(Game.w, "SpamWars");
+			Game.currentFrame.setFullscreen();
+		}
+		catch (IllegalStateException e)
+		{
+			System.exit(0);
+		}
+		Game.currentGame.addLayer(new MenuLayer());
+		
+		Game.currentFrame.updater = new UpdateThread();
+		
+		while (true)
+			Game.currentFrame.main();
 	}
 }
---
+++ src/main/java/de/dakror/spamwars2/settings/CFG.java
@@ -0,0 +1,94 @@
+package de.dakror.spamwars.settings;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+import de.dakror.gamesetup.GameFrame;
+import de.dakror.gamesetup.util.Helper;
+
+/**
+ * @author Dakror
+ */
+public class CFG
+{
+	public static final File DIR = new File(System.getProperty("user.home") + "/.dakror/SpamWars");
+	
+	public static String APP_KEY = "74ff4592-e388-4ccb-a";
+	
+	public static boolean INTERNET;
+	
+	static long time = 0;
+	
+	public static final void init()
+	{
+		DIR.mkdirs();
+		GameFrame.screenshotDir = new File(DIR, "screenshots");
+		
+		
+		File maps = new File(DIR, "maps");
+		maps.mkdir();
+		String[] files = { "BigSnowSurfaceStoneBasementDefault.map", "SmallGrassSurfaceStoneBasementDefault.map" };
+		
+		for (String s : files)
+			Helper.setFileContent(new File(maps, s), Helper.getURLContent(CFG.class.getResource("/map/" + s)));
+	}
+	
+	// -- user settings -- //
+	public static boolean AUTO_RELOAD = true;
+	
+	public static void saveSettings()
+	{
+		File s = new File(DIR, ".properties");
+		try
+		{
+			s.createNewFile();
+			Properties properties = new Properties();
+			properties.setProperty("AUTO_RELOAD", Boolean.toString(AUTO_RELOAD));
+			properties.store(new FileWriter(s), "");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void loadSettings()
+	{
+		File s = new File(DIR, ".properties");
+		if (s.exists())
+		{
+			try
+			{
+				Properties properties = new Properties();
+				properties.load(new FileReader(s));
+				
+				AUTO_RELOAD = properties.getProperty("AUTO_RELOAD").equals("true");
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// -- debug profiling -- //
+	public static void u()
+	{
+		if (time == 0) time = System.currentTimeMillis();
+		else
+		{
+			p(System.currentTimeMillis() - time);
+			time = 0;
+		}
+	}
+	
+	public static void p(Object... p)
+	{
+		if (p.length == 1) System.out.println(p[0]);
+		else System.out.println(Arrays.toString(p));
+	}
+}
